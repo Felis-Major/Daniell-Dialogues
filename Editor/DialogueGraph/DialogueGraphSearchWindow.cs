60d6f2e3464d747711f2ef8273304feee542af34
Robert <robert.edilber@pm.me>
2022-01-13 08:03:33 -0800

    initial commit

---
+++ Editor/DialogueGraph/DialogueGraphSearchWindow.cs
@@ -0,0 +1,107 @@
+﻿using Daniell.Runtime.Systems.DialogueNodes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Daniell.Editor.DialogueNodes
+{
+    public class DialogueGraphSearchWindow : ScriptableObject, ISearchWindowProvider
+    {
+        private EditorWindow _editorWindow;
+        private DialogueGraphView _graphView;
+        private Dictionary<Type, string> _validNodeTypes;
+
+        public void Initialize(EditorWindow editorWindow, DialogueGraphView dialogueGraphView, Dictionary<Type, string> validNodeTypes)
+        {
+            _editorWindow = editorWindow;
+            _graphView = dialogueGraphView;
+            _validNodeTypes = validNodeTypes;
+        }
+
+        private struct NodeCreationContext
+        {
+            public Func<BaseNode> CreateNodeAction { get; private set; }
+
+            public NodeCreationContext(Func<BaseNode> createNodeAction)
+            {
+                CreateNodeAction = createNodeAction;
+            }
+
+            public void CreateNode(out BaseNode node)
+            {
+                node = CreateNodeAction?.Invoke();
+            }
+        }
+
+        public List<SearchTreeEntry> CreateSearchTree(SearchWindowContext context)
+        {
+            // Create search tree entry list
+            List<SearchTreeEntry> searchTreeEntries = new List<SearchTreeEntry>();
+
+            // Add header
+            searchTreeEntries.Add(GetGroup("Create Nodes", 0));
+
+            // Find groups
+            List<string> createdGroups = new List<string>();
+            foreach (KeyValuePair<Type, string> validNodeType in _validNodeTypes)
+            {
+                var groupName = validNodeType.Value;
+                if (!createdGroups.Contains(groupName))
+                {
+                    searchTreeEntries.Add(GetGroup(groupName, 1));
+                    createdGroups.Add(groupName);
+                }
+
+                // Create groups
+                var method = typeof(DialogueGraphSearchWindow).GetMethod(nameof(DialogueGraphSearchWindow.GetEntry), BindingFlags.NonPublic | BindingFlags.Instance);
+                var action = method.MakeGenericMethod(validNodeType.Key);
+                var entry = action.Invoke(this, new object[] { 2 });
+                searchTreeEntries.Add(((SearchTreeEntry)entry));
+            }
+
+            return searchTreeEntries;
+        }
+
+        public bool OnSelectEntry(SearchTreeEntry searchTreeEntry, SearchWindowContext context)
+        {
+            NodeCreationContext userData = (NodeCreationContext)searchTreeEntry.userData;
+            userData.CreateNode(out BaseNode node);
+
+            var worldMousePosition = _editorWindow.rootVisualElement.ChangeCoordinatesTo(_editorWindow.rootVisualElement.parent, context.screenMousePosition - _editorWindow.position.position);
+            var localMousePosition = _graphView.contentViewContainer.WorldToLocal(worldMousePosition);
+
+            node.SetPosition((int)localMousePosition.x, (int)localMousePosition.y);
+
+            return true;
+        }
+
+        #region Search Tree Builder
+
+        private SearchTreeEntry GetEntry<T>(int level) where T : BaseNode, new()
+        {
+            var nodeName = Regex.Replace(typeof(T).ToString(), @"((?<=\p{Ll})\p{Lu})|((?!\A)\p{Lu}(?>\p{Ll}))", " $0");
+            var searchTreeEntry = new SearchTreeEntry(new GUIContent(nodeName));
+            searchTreeEntry.userData = new NodeCreationContext(() =>
+            {
+                var node = _graphView.CreateNode<T>();
+                return node;
+            });
+
+            searchTreeEntry.level = level;
+
+            return searchTreeEntry;
+        }
+
+        private SearchTreeGroupEntry GetGroup(string groupName, int level)
+        {
+            return new SearchTreeGroupEntry(new GUIContent(groupName), level);
+        }
+
+        #endregion
+    }
+}
---
+++ Editor/DialogueGraph/DialogueGraphView.cs
@@ -0,0 +1,110 @@
+using Daniell.Runtime.Systems.DialogueNodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Daniell.Editor.DialogueNodes
+{
+    public class DialogueGraphView : GraphView
+    {
+        private DialogueGraphSearchWindow _dialogueGraphSearchWindow;
+
+        public DialogueGraphView(string name, EditorWindow parent, DialogueFile dialogueFile)
+        {
+            this.name = name;
+
+            this.AddManipulator(new ContentDragger());
+            this.AddManipulator(new SelectionDragger());
+            this.AddManipulator(new RectangleSelector());
+
+            // Add zoom capabilities
+            SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+
+            // Add a grid background
+            GridBackground gridBackground = new GridBackground();
+            Insert(0, gridBackground);
+            gridBackground.StretchToParentSize();
+
+            styleSheets.Add(Resources.Load<StyleSheet>("DialogGraph"));
+            // Create the start node
+            CreateNode<StartNode>();
+
+            _dialogueGraphSearchWindow = ScriptableObject.CreateInstance<DialogueGraphSearchWindow>();
+            _dialogueGraphSearchWindow.Initialize(parent, this, dialogueFile.GetValidNodeTypes());
+            nodeCreationRequest = context => SearchWindow.Open(new SearchWindowContext(context.screenMousePosition), _dialogueGraphSearchWindow);
+        }
+
+        public BaseNode CreateNode<T>() where T : BaseNode, new()
+        {
+            BaseNode node = new T();
+            AddElement(node);
+            node.OnNodeUpdated += Test;
+            return node;
+        }
+
+        private void Test()
+        {
+            Debug.Log("Hello World");
+        }
+
+        public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+        {
+            List<Port> compatiblePorts = new List<Port>();
+
+            ports.ForEach(port =>
+            {
+                if (startPort != port && startPort.node != port.node && startPort.direction != port.direction)
+                {
+                    compatiblePorts.Add(port);
+                }
+            });
+
+            return compatiblePorts;
+        }
+
+        public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+        {
+            evt.menu.AppendAction("Duplicate", (e) =>
+            {
+                // Get middle point
+                Vector2 center = Vector2.zero;
+                int count = 0;
+                foreach (ISelectable selectable in selection.Where(x => x is GraphNode))
+                {
+                    center += ((GraphNode)selectable).GetPosition().position;
+                    count++;
+                }
+                center /= count;
+
+                // Get Selection
+                foreach (ISelectable selectable in selection.Where(x => x is GraphNode))
+                {
+                    var graphNode = (GraphNode)selectable;
+
+                    var nodeData = graphNode.ToNodeData();
+
+                    // Instantiate node using reflection
+                    var method = typeof(DialogueGraphView).GetMethod(nameof(DialogueGraphView.CreateNode));
+                    var action = method.MakeGenericMethod(Type.GetType(nodeData.NodeTypeName));
+                    var node = action.Invoke(this, null);
+
+                    // Load node data
+                    var createdNode = (GraphNode)node;
+                    createdNode.FromNodeData(nodeData);
+
+                    // Get node position
+                    var nodePosition = graphNode.GetPosition().position;
+                    var mousePosition = e.eventInfo.mousePosition;
+
+                    nodePosition += mousePosition - center;
+
+                    createdNode.SetPosition((int)nodePosition.x, (int)nodePosition.y);
+                }
+            });
+        }
+    }
+}
